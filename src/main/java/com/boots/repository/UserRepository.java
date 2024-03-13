e4d24e72144ae0127025b82be2a11cadd99d5661
ivan <ivan7855060@gmail.com>
2024-03-14 00:01:04 +0300

    hotfix

--- src/main/java/com/boots/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boots.repository;
-
-import com.boots.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
